a72327c12df71552faf5aecddd7ea0d611dff07d
Abdelrahman74S <bdalrhmnr927@gmail.com>
2026-03-08 22:12:20 +0200

    feat!: add DATABASE Neon

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,6 +3,7 @@ import os
 
 from pathlib import Path
 import environ
+import dj_database_url
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -93,10 +94,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        ssl_require=True,
+    )
 }
 
 
